c2005dac63cfa55a35d21ed2ea27dbd71353d024
727536869 <39111026+727536869@users.noreply.github.com>
2018-05-09 19:06:51 +0800

    Update ICompanyDao.java

--- src/main/java/com/seven/dao/ICompanyDao.java
+++ src/main/java/com/seven/dao/ICompanyDao.java
@@ -11,17 +11,6 @@ import java.util.List;
 
 import com.seven.model.CompanyUser;
 
-/** 
- * <pre>项目名称：seven-group    
- * 类名称：ICompanyDao    
- * 类描述：    
- * 创建人：JavaScript 
- * 创建时间：2018年3月27日 下午2:19:16    
- * 修改人：JavaScript  
- * 修改时间：2018年3月27日 下午2:19:16    
- * 修改备注：       
- * @version </pre>    
- */
 public interface ICompanyDao {
 
 
